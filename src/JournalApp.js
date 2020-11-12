313084cde08b8562e378f52f9f4c98aba1223618
Emmanuel Guzman <emmanuel-a-guzman@hotmail.com>
2020-11-12 19:55:46 -0300

    Redux -store- Login - Logout(reducer) types

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -5,8 +5,8 @@ import { store } from "./store/store";
 
 export const JournalApp = () => {
   return (
-    // <Provider store={store}>
+     <Provider store={store}>
       <AppRouter />
-    // </Provider> 
+     </Provider> 
   );
 };
